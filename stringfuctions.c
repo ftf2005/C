1bb55e40763b33da9a126e09466325b0a9eff7aa
ftf2005 <frickthefasak2@outlook.com>
2024-08-07 09:09:27 +0000

    ok

--- stringfuctions.c
+++ stringfuctions.c
@@ -3,17 +3,15 @@
 #include<string.h>
 void main()
 {
-    char str[100],str1[100],str2[100];
-    int i;
+    char str[100],str1[100];
     printf("Enter the string:");
     scanf("%s",str);
-    printf("Length of string is:%d\n",strlen(str));
+    printf("Length of string is:%d \n",strlen(str));
     printf("Enter the string to be copied:");
     scanf("%s",str1);
-    strcpy(str2,str1);
-    printf("Copied string is:%s\n",str2);
-    printf("Enter the string to be concatenated:");
-    scanf("%s",str1);
-    strcat(str2,str1);
-    printf("Concatenated string is:%s\n",str2);
+    strcpy(str1,str);
+    printf("Copied string is:%s\n",str1);
+    printf("Concatenated string is:%s\n",  strcat(str1,str));
+    // printf("reverse:%d\n",strrev(str));
+
 }
